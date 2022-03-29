8140a1cd8df165aabb2c43a0461574a54d8aef6a
Samra Raković <samra.rakovic0@gmail.com>
2022-03-29 22:27:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("I like books!");
+console.log("Hello everyone!"); 
+
+window.alert("I really love to read books!!!");
+
+//This is a comment
+
+/* 
+ This is a multiline comment 
+*/
+
+// Variables
+let firstName = "Samra";
+let age = 21; 
+let student = true; 
+
+console.log("Hello", firstName);
+console.log("You are", age, "years old");
+console.log("Enrolled:", student);
+
+document.getElementById("p1").innerHTML = "Hello " + firstName;
+document.getElementById("p2").innerHTML = "You are " + age + " years old";
