268af3252230860873b35ed2d89d4a7b2c687ecf
Samra Raković <samra.rakovic0@gmail.com>
2022-03-30 23:11:20 +0200

    User input/type conversion js - label/input/button html

--- index.js
+++ index.js
@@ -41,3 +41,34 @@ console.log(studentsArithmetic);
 /* operator precedence */
 let result = (1 + 2) * (3 + 4);
 console.log(result);
+
+//User input
+    // Easy way with a window prompt
+        //let username = window.prompt("What's your name?");
+        //console.log(username);
+    // Difficult way HTML textbox
+    let username;
+    document.getElementById("myButton").onclick = function() {
+        username = document.getElementById("myText").value; 
+        console.log(username);
+        document.getElementById("myLabel").innerHTML = "Hello " + username;
+    }
+
+//Type conversion
+    /*let ageConversion = window.prompt("How old are you?"); 
+    console.log(typeof ageConversion);
+    ageConversion = Number(ageConversion);
+    console.log(typeof ageConversion);
+    ageConversion += 1;
+    console.log("Happy Birthday! You are", ageConversion, "years old");*/
+    let x; 
+    let y; 
+    let z;
+
+    x = Number("3.14");
+    y = String(3.14);
+    z = Boolean("pizza"); 
+
+    console.log(x, typeof x);
+    console.log(y, typeof y);
+    console.log(z, typeof z);
