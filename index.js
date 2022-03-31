f9812ddc1fa044c2fc8c7ac8bf8ce77ea47dc907
Samra Raković <samra.rakovic0@gmail.com>
2022-04-01 01:37:43 +0200

    Js/Html/Css

--- index.js
+++ index.js
@@ -138,3 +138,19 @@ document.getElementById("submitButton").onclick = function() {
     document.getElementById("cLabel").innerHTML = "Side C: " + c;
 }
 
+//count
+
+let count = 0; 
+
+document.getElementById("decreaseBtn").onclick = function() {
+    count -=1;
+    document.getElementById("countLabel").innerHTML =  count;
+}
+document.getElementById("resetBtn").onclick = function() {
+    count =0;
+    document.getElementById("countLabel").innerHTML =  count;
+}
+document.getElementById("increaseBtn").onclick = function() {
+    count +=1;
+    document.getElementById("countLabel").innerHTML =  count;
+}
